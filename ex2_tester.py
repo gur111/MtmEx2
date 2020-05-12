2bfb9ef3bdaf515188f352737919a30c7f3dd0bd
Gur Telem <gur111telem@gmail.com>
2020-05-13 01:11:02 +0300

    Tester with colors now

--- ex2_tester.py
+++ ex2_tester.py
@@ -1,7 +1,21 @@
 import hw2
-
+import os
+import sys
+from io import StringIO
+from termcolor import colored
 
 if __name__ == '__main__':
-    entries = hw2.readParseData('test1.txt')
-    champs = hw2.calcCompetitionsResults(entries)
+    stdout_stream = StringIO()
+
+    with open("out1.txt") as f:
+        expected  = f.read()
+
+    stdout, sys.stdout = sys.stdout, stdout_stream
+
+    hw2.partA("test1.txt")
+    sys.stdout = stdout
+    stdout_stream.seek(0)
+    actual = stdout_stream.read()
+
+    print(f'Test 1: {colored("passed", "green") if actual == expected[:len(actual)] else colored("failed", "red")}')
     print('Hello world')
